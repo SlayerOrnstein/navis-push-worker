7c604e27a8193839c3bcf1dcf49831dfd3da5517
SlayerOrnstein <6075693+SlayerOrnstein@users.noreply.github.com>
2024-02-23 11:43:08 -0500

    fix: changes for new icon path

--- lib/src/services/auth.dart
+++ lib/src/services/auth.dart
@@ -23,7 +23,7 @@ class Auth {
 
   static final _androidNotification = AndroidNotification()
     ..color = '#1565c0ff'
-    ..icon = 'ic_nightmare';
+    ..icon = 'ic_launcher';
 
   static final _androidConfig = AndroidConfig()
     ..priority = 'HIGH'
