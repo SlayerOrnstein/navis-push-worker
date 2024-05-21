1ff8a5068fefc98f71eb95adc18a2b3ab0b45843
SlayerOrnstein <6075693+SlayerOrnstein@users.noreply.github.com>
2024-05-21 08:38:09 -0400

    feat: enable duviri

--- bin/main.dart
+++ bin/main.dart
@@ -1,27 +1,32 @@
 import 'dart:async';
+import 'dart:io';
 
 import 'package:mason_logger/mason_logger.dart';
 import 'package:navis_push_worker/handlers.dart';
 import 'package:navis_push_worker/services.dart';
 import 'package:warframestat_client/warframestat_client.dart';
 
+const delay = Duration(seconds: 60);
+
 Future<void> main() async {
   final logger = Logger()..info('Starting push_server');
-  final client = WorldstateClient();
-  final auth = await Auth.initialize();
-  final cache = await MessageIdCache.init();
 
   try {
+    final client = WarframestatWebsocket.connect();
+    final auth = await Auth.initialize();
+    final cache = await MessageIdCache.init();
+
     logger.info('Listening to websocket');
 
-    Timer.periodic(const Duration(minutes: 1), (timer) async {
-      final worldstate = await client.fetchWorldstate();
-      await sendNotifications(worldstate, auth, cache);
-    });
+    client
+        .worldstateEvents()
+        .distinct((p, n) => p.timestamp.difference(n.timestamp).abs() >= delay)
+        .listen((w) => sendNotifications(w, auth, cache));
 
     logger.info('Started push_server');
   } catch (e) {
     logger.err(e.toString());
+    exit(1);
   }
 }
 
@@ -35,7 +40,7 @@ Future<void> sendNotifications(
     BaroHandler(state.voidTraders, auth, cache),
     CetusHandler(state.cetusCycle, auth, cache),
     DarvoDealHandler(state.dailyDeals, auth, cache),
-    // DuviriHandler(state.duviriCycle),
+    // DuviriHandler(state.duviriCycle, auth, cache),
     EarthHandler(state.earthCycle, auth, cache),
     CambionHandler(state.cambionCycle, auth, cache),
     Invasionhandler(state.invasions, auth, cache),
