e60fea8a2992101ae2ddc0c9cb41f372ca36cd11
SlayerOrnstein <6075693+SlayerOrnstein@users.noreply.github.com>
2024-06-19 17:14:39 -0400

    fix: light tweak

--- lib/src/message_handlers/alert_handler.dart
+++ lib/src/message_handlers/alert_handler.dart
@@ -22,7 +22,8 @@ class AlertHandler extends MessageHandler {
       final notification = Notification()
         ..title = alert.mission.node
         ..body = '${alert.mission.type} (${alert.mission.faction})'
-            ' | ${alert.mission.minEnemyLevel} - ${alert.mission.maxEnemyLevel}'
+            ' | Level ${alert.mission.minEnemyLevel} -'
+            ' ${alert.mission.maxEnemyLevel}'
             ' | ${alert.mission.reward!.itemString}';
 
       await auth.send(NotificationKeys.alertsKey, notification);
